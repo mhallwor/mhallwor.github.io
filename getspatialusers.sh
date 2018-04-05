b3347cddf8f97f3826bf275752cd6a0eed3a6a68
mhallwor <36464198+mhallwor@users.noreply.github.com>
2018-04-05 15:12:06 -0400

    [ci skip] welcome_AWS.r copy

--- getspatialusers.sh
+++ getspatialusers.sh
@@ -42,8 +42,8 @@ for id in $(seq 1 $1); do
         --shell /bin/false \
         --disabled-password \
         getspatial$id < /dev/null
-    #cp -a /home/rstudio/ $userhome
-    #chown -R getspatial$id $userhome
+    cp /home/rstudio/welcome_AWS.r $userhome/welcome_AWS.r
+    chown -R getspatial$id $userhome
     echo "getspatial$id:AOS18AZ" | chpasswd
 done
 
